b59f70eebc24da92fe78dd91ed1afef96aedbe82
tanishq1504 <149486313+tanishq1504@users.noreply.github.com>
2023-12-21 20:02:15 +0000

    data types

---
+++ 01_Basics/02.DataTypes.js
@@ -0,0 +1,15 @@
+let name ="Tanishq"
+let age = 19
+let male=true
+
+
+
+// number
+// string "" or ''
+//boolean Ture or False 
+//null stand alone video
+//undefined 
+//symbol => unique 
+//object 
+
+console.log(typeof age);
