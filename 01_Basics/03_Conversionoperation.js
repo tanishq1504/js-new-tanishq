8c649347f5ae34bcf5e9ab5bb75b40987344b7a4
tanishq1504 <149486313+tanishq1504@users.noreply.github.com>
2023-12-21 20:17:19 +0000

    operation

--- 01_Basics/03_Conversionoperation.js
+++ 01_Basics/03_Conversionoperation.js
@@ -11,3 +11,19 @@ console.log(score)
 // True => 1; false => 0
 //null => 1\
 
+//*********************operations ********************************
+
+let value = 3 
+let nagvalue = -value 
+console.log(nagvalue)
+
+
+// ** for power 
+//2%3 for reamiander
+
+
+let str1 ="hello"
+let str2 ="Tanishq"
+let str3 ="Hii"
+let str4 = str1+str2+str3
+console.log(str4);
